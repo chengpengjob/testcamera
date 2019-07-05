c4cf9d545ffbff4ae4b01a80dbdc1df1ab4a8497
chengpeng <chengpeng@cen-speed.com>
2019-07-05 15:45:05 +0800

    关闭播放页面，切片终止，删除已生成的切片节省资源

--- modules/web/src/com/company/testcamera/web/controller/CameraBrowseController.java
+++ modules/web/src/com/company/testcamera/web/controller/CameraBrowseController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
 @RestController
 @RequestMapping("/controller")
 public class CameraBrowseController {
@@ -14,5 +16,21 @@ public class CameraBrowseController {
     public void teardownCamById(@PathVariable Integer id){
         SyncPipe syncPipe =(SyncPipe) MidleMap.getnum(id);
         syncPipe.cancel();
+
+        String fileDir = "F:/testcamera/deploy/tomcat/webapps";
+        File dir = new File(fileDir+"/HLS-demo/m3u8/Gear" + id);
+    }
+
+    private static void removeDir(File dir){
+        File[] files = dir.listFiles();
+        for(File file : files){
+            if(file.isDirectory()){
+                removeDir(file);
+            }else{
+                System.out.println(file+":"+file.delete());
+            }
+
+        }
+        System.out.println(dir+":"+dir.delete());
     }
 }
